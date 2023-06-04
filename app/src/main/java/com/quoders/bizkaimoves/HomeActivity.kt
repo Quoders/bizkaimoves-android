52533a6b3eafaf1034315797ba4f6d5b6a65190c
David Guerrero <david.guerrero@quoders.com>
2023-06-04 14:11:40 +0100

    Migrate Groovy to Kotlin

--- app/src/main/java/com/quoders/bizkaimoves/HomeActivity.kt
+++ app/src/main/java/com/quoders/bizkaimoves/HomeActivity.kt
@@ -3,17 +3,7 @@ package com.quoders.bizkaimoves
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.quoders.bizkaimoves.ui.BizkaimovesApp
-import com.quoders.bizkaimoves.ui.theme.BizkaimovesTheme
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
