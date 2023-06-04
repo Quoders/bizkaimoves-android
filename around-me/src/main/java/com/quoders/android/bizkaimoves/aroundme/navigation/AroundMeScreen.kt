e3c68b395c24a8f84bc9fc4d1b0c23ed2bfac9d0
David Guerrero <david.guerrero@quoders.com>
2023-06-04 16:26:00 +0100

    Add libs catalog

--- around-me/src/main/java/com/quoders/android/bizkaimoves/aroundme/navigation/AroundMeScreen.kt
+++ around-me/src/main/java/com/quoders/android/bizkaimoves/aroundme/navigation/AroundMeScreen.kt
@@ -1,7 +1,5 @@
 package com.quoders.android.bizkaimoves.aroundme.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,23 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
 }
+
 rootProject.name = "bizkaimoves-android"
-include ':app'
-include ':around-me'
-include ':lines'
-include ':favorites'
+include(":app")
+include(":around-me")
+include(":lines")
+include(":favorites")
