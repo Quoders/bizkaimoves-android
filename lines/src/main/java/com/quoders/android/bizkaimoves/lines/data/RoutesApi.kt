2f68e2f2cc288441e09933c1ac628ec7d6f8aff7
David Guerrero <david.guerrero@quoders.com>
2023-10-30 19:15:49 +0000

    Update RoutesApi.kt

--- lines/src/main/java/com/quoders/android/bizkaimoves/lines/data/RoutesApi.kt
+++ lines/src/main/java/com/quoders/android/bizkaimoves/lines/data/RoutesApi.kt
@@ -7,34 +7,38 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+data class RouteData(
+    val route_id: Int,
+    val route_short_name: String,
+    val route_long_name: String,
+)
+
 interface RoutesApiService {
     @GET("api/routes")
-    suspend fun getRoutes(): RoutesApiData
+    suspend fun getRoutes(): List<RouteData>
 }
 
 class RoutesApi : RoutesApiService {
+    private val logging = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
 
     private val networkApi = Retrofit.Builder()
         .baseUrl(bizkaibusBaseUrlLocalHost)
         .addConverterFactory(GsonConverterFactory.create())
-        .client(
-            OkHttpClient.Builder()
-                .addInterceptor(
-                    HttpLoggingInterceptor()
-                        .apply {
-                            setLevel(HttpLoggingInterceptor.Level.BODY)
-                        },
-                )
-                .build()
-        )
+        .client(client)
         .build()
         .create(RoutesApiService::class.java)
 
-    override suspend fun getRoutes(): RoutesApiData = networkApi.getRoutes()
+    override suspend fun getRoutes() = networkApi.getRoutes()
 }
 
-data class NetworkResponse<T>(
-    val data: T,
-)
-
-private const val bizkaibusBaseUrlLocalHost = "http://10.0.2.2:4000"
+private const val bizkaibusBaseUrlLocalHost = "https://bizkaibus-api-52b009d032a6.herokuapp.com"
